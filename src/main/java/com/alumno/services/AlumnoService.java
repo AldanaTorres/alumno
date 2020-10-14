47efa34a2f1591aa6d229def4f0fb2c460858eba
Aldana Torres <aldanaatorres@gmail.com>
2020-10-13 23:14:49 -0300

    Service y serviceImplementacion listo

--- src/main/java/com/alumno/services/AlumnoService.java
+++ src/main/java/com/alumno/services/AlumnoService.java
@@ -1,5 +1,12 @@
 package com.alumno.services;
 
-public class AlumnoService {
+import java.util.List;
+
+public interface AlumnoService<Alumno>{
     //Aldana
+    public List<Alumno> findAll() throws Exception;
+    public Alumno findById(Long id) throws Exception;
+    public Alumno save(Alumno entity) throws Exception;
+    public Alumno update(Long id, Alumno entity) throws Exception;
+    public boolean delete(Long id) throws Exception;
 }
---
+++ src/main/java/com/alumno/services/AlumnoServiceImpl.java
@@ -0,0 +1,72 @@
+package com.alumno.services;
+
+import com.alumno.entities.Alumno;
+import com.alumno.repository.AlumnoRepository;
+
+import javax.persistence.Table;
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+public class AlumnoServiceImpl implements AlumnoService<Alumno>{
+
+    protected AlumnoRepository<Alumno,Long> alumnoRepository;
+    @Override
+    @Transactional
+    public List<Alumno> findAll() throws Exception {
+        try{
+            List<Alumno> entities = alumnoRepository.findAll();
+            return entities;
+        }catch (Exception e){
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    @Override
+    @Transactional
+    public Alumno findById(Long id) throws Exception {
+        try{
+            Optional<Alumno> entityOptional = alumnoRepository.findById(id);
+            return entityOptional.get();
+        }catch (Exception e){
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    @Override
+    public Alumno save(Alumno entity) throws Exception {
+        try {
+            entity = alumnoRepository.save(entity);
+            return entity;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    @Override
+    public Alumno update(Long id, Alumno entity) throws Exception {
+        try {
+            Optional<Alumno> entityOptional = alumnoRepository.findById(id);
+            Alumno alumno = entityOptional.get();
+            alumno = alumnoRepository.save(entity);
+            return alumno;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    @Override
+    public boolean delete(Long id) throws Exception {
+        try {
+            if(alumnoRepository.existsById(id)){
+                alumnoRepository.deleteById(id);
+                return true;
+            } else {
+                throw new Exception();
+            }
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+
+    }
+}
