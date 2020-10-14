dbb4a5b5f273617c81580c4d42c47e331347db5f
emispalenza <emispalenza@gmail.com>
2020-10-13 23:32:28 -0300

    Controller

--- src/main/java/com/alumno/controllers/AlumnoController.java
+++ src/main/java/com/alumno/controllers/AlumnoController.java
@@ -1,5 +1,66 @@
 package com.alumno.controllers;
 
-public class AlumnoController {
-    //Emi
+
+import com.alumno.entities.Alumno;
+import com.alumno.services.AlumnoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping(path = "api/v1/alumnos")
+public class AlumnoController extends Alumno {
+     //Emi
+     private AlumnoService alumnoService;
+
+    public AlumnoController(AlumnoService alumnoService){
+        this.alumnoService = alumnoService;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<?> getAll(){
+        try{
+            return ResponseEntity.status(HttpStatus.OK).body(alumnoService.findAll());
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"Error. Por favor intente más tarde.\"}");
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getOne(@PathVariable Long id){
+        try{
+            return ResponseEntity.status(HttpStatus.OK).body(alumnoService.findById(id));
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"Error. Por favor intente más tarde.\"}");
+        }
+    }
+
+    @PostMapping("")
+    public ResponseEntity<?> save(@RequestBody Alumno entity){
+        try{
+            return ResponseEntity.status(HttpStatus.OK).body(alumnoService.save(entity));
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error. Por favor intente más tarde.\"}");
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id,@RequestBody Alumno entity){
+        try{
+            return ResponseEntity.status(HttpStatus.OK).body(alumnoService.update(id, entity));
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error. Por favor intente más tarde.\"}");
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        try{
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(alumnoService.delete(id));
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error. Por favor intente más tarde.\"}");
+        }
+    }
+
 }
