c760c81d3642b56e12c1d13e79be1c290ce4df9a
Cristian Sacca <cristiansacca97@gmail.com>
2020-10-13 22:40:06 -0300

    Creacion AlumnoRepository

--- src/main/java/com/alumno/repository/AlumnoRepository.java
+++ src/main/java/com/alumno/repository/AlumnoRepository.java
@@ -1,5 +1,10 @@
 package com.alumno.repository;
 
-public class AlumnoRepository {
-    //Cristian
+import com.alumno.entities.Alumno;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlumnoRepository extends JpaRepository<Alumno, Long>{
+
 }
