e8e3c0e611c8f29c782d83f554fbf4c20f00e279
GermanAmico <germanamico@hotmail.com>
2020-10-13 22:46:42 -0300

    implementacion serializable

--- src/main/java/com/alumno/entities/Alumno.java
+++ src/main/java/com/alumno/entities/Alumno.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.util.Date;
 
 
@@ -15,7 +16,7 @@ import java.util.Date;
 @AllArgsConstructor
 @Getter
 @Setter
-public class Alumno {
+public class Alumno implements Serializable {
     //German
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
