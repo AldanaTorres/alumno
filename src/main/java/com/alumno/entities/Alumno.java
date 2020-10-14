a60f232b38ac5352b33bf0b4f896803f34ac4401
GermanAmico <germanamico@hotmail.com>
2020-10-13 22:05:30 -0300

    Creacion de entidad alumno

--- src/main/java/com/alumno/entities/Alumno.java
+++ src/main/java/com/alumno/entities/Alumno.java
@@ -1,5 +1,29 @@
 package com.alumno.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+
+@Entity
+@Table(name = "alumno")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Alumno {
     //German
+    @Column(name="nombre")
+    private String nombre;
+    @Column(name="apellido")
+    private String apellido;
+    @Column(name="email")
+    private String email;
+    @Column(name = "create_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createAt;
 }
